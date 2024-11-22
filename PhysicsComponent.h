97deafb56b36cf2724e29fca056492ca953fd33d
dummy-ie <115205154+dummy-ie@users.noreply.github.com>
2024-11-22 19:51:10 +0800

    PhysicsComponent getters and setters

--- PhysicsComponent.h
+++ PhysicsComponent.h
@@ -8,9 +8,25 @@ namespace GDEngine
 	using namespace reactphysics3d;
 	class PhysicsComponent : public AComponent
 	{
+	public:
+		enum class EConstraints : uint8_t
+		{
+			None = 0,
+			FreezePositionX = 1 << 0,
+			FreezePositionY = 1 << 1,
+			FreezePositionZ = 1 << 2,
+			FreezeRotationX = 1 << 3,
+			FreezeRotationY = 1 << 4,
+			FreezeRotationZ = 1 << 5,
+			FreezePosition = FreezePositionX | FreezePositionY | FreezePositionZ,
+			FreezeRotation = FreezeRotationX | FreezeRotationY | FreezeRotationZ,
+			FreezeAll = FreezePosition | FreezeRotation
+		};
+
 	private:
-		float m_mass = 1000.0f;
 		RigidBody* m_rigidBody;
+		float m_mass = 1000.0f;
+		uint8_t m_constraints = 0;
 
 	public:
 		PhysicsComponent(std::string name, AGameObject* owner);
@@ -19,5 +35,19 @@ namespace GDEngine
 	public:
 		void perform(float deltaTime) override;
 		RigidBody* getRigidBody();
+
+		float getMass();
+		bool getUseGravity();
+		BodyType getType();
+		float getLinearDrag();
+		float getAngularDrag();
+		bool getConstraint(EConstraints constraint);
+
+		void setMass(float mass);
+		void setUseGravity(bool isUsingGravity);
+		void setType(BodyType type) const;
+		void setLinearDrag(float linearDrag);
+		void setAngularDrag(float angularDrag);
+		void setConstraints(EConstraints constraints);
 	};
 }
